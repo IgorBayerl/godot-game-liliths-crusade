85f8fe5011ba8f5a26abc8ac691b871b2014f441
Igor Bayerl <bayerl.igor@gmail.com>
2020-08-17 12:11:12 -0300

    Commit

---
+++ src/Scripts/Player.gd
@@ -0,0 +1,55 @@
+extends KinematicBody2D
+
+export var SPEED = 300
+export var GRAVITY = 50
+export var JUMP_FORCE = -300
+
+const NORMAL = Vector2(0, -1)
+
+var motion = Vector2()
+var direction = Vector2()
+
+
+func _physics_process(delta: float) -> void:
+	_direction_move(delta)
+	
+	
+func _direction_move(delta):
+	
+	direction = Vector2()
+	motion.y += GRAVITY
+	
+	
+	if Input.is_action_pressed("move_UP"):
+#		motion.y = -SPEED 
+		direction += Vector2(0,1)
+	elif Input.is_action_pressed("move_DOWN"):
+#		motion.y = SPEED 
+		direction += Vector2(0,-1)
+	else:
+		direction += Vector2(0,0)
+		
+	if Input.is_action_pressed("move_RIGHT"):
+		motion.x = SPEED 
+		direction += Vector2(1 ,0)
+	elif Input.is_action_pressed("move_LEFT"):
+		motion.x = -SPEED 
+		direction += Vector2(-1 ,0)
+	else:
+		direction += Vector2(0,0)
+		motion.x = 0
+		
+		
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		var gravidade_anterior = GRAVITY
+		if Input.is_action_pressed("jump"):
+			motion.y = JUMP_FORCE
+		
+	motion = move_and_slide(motion  , NORMAL)
+#	print(direction)
+	
+func _pode_pular() -> bool:
+	if is_on_floor():
+		return true
+	else:
+		return false
