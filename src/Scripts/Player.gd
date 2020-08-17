9f6e83f2c145cebfab542f50a9fba59ad2d6313a
Igor Bayerl <bayerl.igor@gmail.com>
2020-08-17 12:51:39 -0300

    commit

--- src/Scripts/Player.gd
+++ src/Scripts/Player.gd
@@ -41,9 +41,10 @@ func _direction_move(delta):
 		
 		
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		var gravidade_anterior = GRAVITY
+		var gravidade_save = GRAVITY
 		if Input.is_action_pressed("jump"):
 			motion.y = JUMP_FORCE
+		
 		
 	motion = move_and_slide(motion  , NORMAL)
 #	print(direction)
