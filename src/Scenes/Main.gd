72cfee7f0cff46e9d1385cef354720a04205cfd2
Igor Bayerl <bayerl.igor@gmail.com>
2020-08-16 22:26:39 -0300

    aloooo

--- src/Scenes/Main.gd
+++ src/Scenes/Main.gd
@@ -8,10 +8,10 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	print('helloooooo')
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-alooooooo
+
