58128ec2348aa4092afe2e067ce5e94b7aa98571
Igor Bayerl <bayerl.igor@gmail.com>
2020-08-22 22:45:39 -0300

    commi te

--- src/Scripts/Health Bar.gd
+++ src/Scripts/Health Bar.gd
@@ -1,14 +1,27 @@
 extends Control
 
-onready var health_bar = $HealthBar
+onready var health_over = $HealthOver
 onready var health_under = $HealthUnder
 onready var update_tween = $UpdateTween
 
+var health = 100
+
 func _on_health_updated(health, amount):
-#	health_bar.value = health
+	health_over.value = health
 	update_tween.interpolate_property(health_under, "value", health_under.value, health, 0.4, Tween.TRANS_SINE, Tween.EASE_IN_OUT, 0.4)
 	update_tween.start()
 	
 func _on_max_health_updated(max_health):
-	health_bar.max_value = max_health
+	health_over.max_value = max_health
 	health_under.max_value = max_health
+
+
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("interact"):
+		print("alooooo")
+		health = health - 15
+		_on_health_updated(health,0)
+
+func _ready() -> void:
+	_on_health_updated(health,0)
