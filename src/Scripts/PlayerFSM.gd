3bf0340a8aead55470851737aee34f710148a623
Igor Bayerl <bayerl.igor@gmail.com>
2020-09-06 13:32:04 -0300

    cleaning the code

--- src/Scripts/PlayerFSM.gd
+++ src/Scripts/PlayerFSM.gd
@@ -9,11 +9,9 @@ const UP = Vector2(0, -1)
 const SLOPE_STOP = 60
 const WALL_JUMP_VELOCITY = Vector2(400 , -500)
 
-var can_dash = true
-
 ####################
-var move_speed = 300
 
+var move_speed = 300
 var velocity = Vector2()
 var move_direction
 var move_input_speed = 0
@@ -31,7 +29,6 @@ var can_atack = true
 var max_jump_velocity = -600
 var min_jump_velocity = -400
 
-
 # STATES #
 
 var is_jumping = false
@@ -41,8 +38,6 @@ var is_dead = false
 var is_atacking = false
 var is_rolling = false
 var is_crouched = false
-
-
 
 ############
 
@@ -58,37 +53,37 @@ func _apply_gravity(delta):
 	velocity.y += GRAVITY * delta
 	if is_jumping and velocity.y >=0:
 		is_jumping = false
-	
+
 func _cap_gravity_wall_slide():
 	var max_velocity = 96
 	velocity.y = min(velocity.y, max_velocity)
-	
+
 func _handle_wall_slide_sticking():
 	if move_direction !=0 and move_direction != wall_direction:
 		if wall_slide_sticky_timer.is_stopped():
 			wall_slide_sticky_timer.start()
 	else:
 		wall_slide_sticky_timer.stop()
-	
+
 func _wall_jump():
 	var wall_jump_velocity = WALL_JUMP_VELOCITY
 	wall_jump_velocity.x *= -wall_direction
 	velocity = wall_jump_velocity
 	wal_jumping = true
 	$WalljumpMovementBlocker.start()
-	
+
 func _apply_movement():
 	velocity = move_and_slide(velocity, UP,SLOPE_STOP)
-	
+
 func _update_move_direction():
 	move_direction = -int(Input.is_action_pressed("move_LEFT")) + int(Input.is_action_pressed("move_RIGHT"))
-	
+
 func _handle_move_input():
 	velocity.x = lerp(velocity.x, move_speed * move_direction, _get_h_weight())
 	if move_direction != 0:
 		player_body.scale.x = move_direction
 		facing = move_direction
-	
+
 func _get_h_weight():
 	if is_on_floor():
 		return 0.2
@@ -99,7 +94,7 @@ func _get_h_weight():
 			return 0.0
 		else:
 			return 0.1
-	
+
 func _update_wall_direction():
 	var is_near_wall_left = _check_is_valid_wall(left_wall_raycasts)
 	var is_near_wall_right = _check_is_valid_wall(right_wall_raycasts)
@@ -108,7 +103,7 @@ func _update_wall_direction():
 		wall_direction = move_direction
 	else:
 		wall_direction = -int(is_near_wall_left) + int(is_near_wall_right)
-	
+
 func _check_is_valid_wall(wall_raycasts):
 	for raycast in wall_raycasts.get_children():
 		if raycast.is_colliding():
@@ -116,12 +111,12 @@ func _check_is_valid_wall(wall_raycasts):
 			if dot > PI * 0.35 and dot < PI * 0.55:
 				return true
 	return false
-	
+
 func _set_head_direction():
 	head_direction.x = 1.5
 	head_direction.y = -int(Input.is_action_pressed("move_DOWN")) + int(Input.is_action_pressed("move_UP"))
 	$SPRITES/Head.rotation = -head_direction.angle()
-	
+
 func _roll():
 	velocity.x = facing * rolling_speed
 
@@ -160,43 +155,23 @@ func _atack_combo():
 		elif atack_combo == 2:
 			anim_player.play("Atack2")
 
-		
 func camera_shake(timeout):
 	$Mira/Camera_position/Camera2D.shake = true
 	yield(get_tree().create_timer(timeout), "timeout")
 	$Mira/Camera_position/Camera2D.shake = false
-	
+
 func take_damage(damage):
 	get_parent().get_node("CanvasLayer/Control/Health Bar").take_damage(damage)
 	health -= damage
 	death_detection()
-	
+
 func death_detection():
 	if health <= 0 and not is_dead:
 		is_dead = true
 		emit_signal("OnDeath",self)
 
 
-################## DASH ###################
 
-func dash():
-	$Dash_sound.play()
-	is_rolling = true
-	can_dash = false
-
-	move_speed = 1000
-	$Dash.visible = false
-############################################
-func _on_Ghost_Timer_timeout() -> void:
-	pass
-#	if is_dashing:
-#		var this_ghost = preload("res://src/Actors/Efeitos/ghost.tscn").instance()
-#		get_parent().add_child(this_ghost)
-#		this_ghost.position = position + Vector2(0,20)
-#		this_ghost.texture = $SPRITES/body.frames.get_frame($SPRITES/body.animation, $SPRITES/body.frame)
-###########################################
-#func animations_set():
-#
 #	######## MILI ATACK ########
 #	if is_atacking != 0:
 #		$Mira/Arma.visible = false
@@ -243,8 +218,6 @@ func _on_WalljumpMovementBlocker_timeout() -> void:
 func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
 	if anim_name == "Atack1":
 		is_atacking = false
-
-
 
 func _on_Roll_Timer_timeout() -> void:
 	is_rolling = false
